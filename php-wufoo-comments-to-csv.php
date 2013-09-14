69ac82b889c4de74fc198706cba48741f4d9f2f5
Chris Stringer <chris@chrisstringer.us>
2013-09-14 11:23:31 -0500

    Adds error reporting, header and comments

--- php-wufoo-comments-to-csv.php
+++ php-wufoo-comments-to-csv.php
@@ -1,4 +1,8 @@
 <?php
+/***
+ php-wufoo-comments-to-csv.php - Create CSV of Wufoo form comments
+ $ rev 1.0.6, 2013-09-14 11:16, cstringer42@gmail.com $
+ */
 define ("PAGE_SIZE", 100);
 define ("URL_BASE",  'https://[YOURSITE].wufoo.com/api/v3/forms');
 define ("FORM_HASH", '[YOURFORMID]');
@@ -9,45 +13,59 @@ define ("FNAME_EXT", '.csv');
 
 date_default_timezone_set ('America/Chicago');
 
-// init cURL lib
+/* init cURL lib */
 $curl = curl_init();
 curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt ($curl, CURLOPT_USERPWD, USERPWD);
 curl_setopt ($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt ($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt ($curl, CURLOPT_FOLLOWLOCATION, TRUE);
 curl_setopt ($curl, CURLOPT_USERAGENT, USERAGENT);
 
-// get total number of comments
+/* Get total number of comments */
 $comment_count = 0;
-curl_setopt ($curl, CURLOPT_URL, URL_BASE . '/' . FORM_HASH . '/comments/count.json');
+// fetch comment count JSON feed
+$cc_url = URL_BASE . '/' . FORM_HASH . '/comments/count.json';
+curl_setopt ($curl, CURLOPT_URL, $cc_url);
 $cc_rj = curl_exec ($curl);
 $res_stat = curl_getinfo ($curl);
 if ($res_stat['http_code'] != 200)
   {
+  echo "ERROR: Code " . $res_stat['http_code'] . " fetching comment count.\n";
   exit();
   }
+// decode JSON to PHP obj
 $ccrj_data = json_decode ($cc_rj);
+// extraxt comment count
 $comment_count = $ccrj_data->{'Count'};
 if ($comment_count == 0)
   {
+  echo "ERROR: No comments attached to form " . FORM_HASH . "\n";
   exit();
   }
 echo "Total comments to process: $comment_count\n";
 
-// get comment data as JSON string
+/* Get comment data as JSON string
+   NOTE: Since comments are returned in a limited page size,
+	 this steps through by PAGE_SIZE entries, creating one
+	 string of the JSON data as a single ordered array ($res_json)
+*/
 $res_json = "";
 for ($page = 0; $page <= $comment_count; $page += PAGE_SIZE)
   {
+  // build the query string, including page size and starting page number
   $qstr = '?pageSize=' . PAGE_SIZE . '&pageStart=' . $page;
 
+  // build URL to REST endpoint
   $pg_url = URL_BASE . '/' . FORM_HASH .'/comments.json' . $qstr;
   curl_setopt ($curl, CURLOPT_URL, $pg_url);
 
+  // execute request
   $rj = curl_exec ($curl);
   $res_stat = curl_getinfo ($curl);
   if ($res_stat['http_code'] != 200)
     {
+	 echo "ERROR: Code " . $res_stat['http_code'] . " fetching comment page $page.\n";
     exit();
     }
 
@@ -73,7 +91,7 @@ if (count ($res_data) != $comment_count)
   }
 echo "Decoded " . count ($res_data) . " of " . $comment_count . " comments.\n";
 
-// save results
+// create CSV file
 $fname = FNAME_BASE . '_' . date ("Y-m-d") . FNAME_EXT;
 $fh = fopen ($fname, 'w');
 if ($fh == FALSE)
@@ -82,6 +100,7 @@ if ($fh == FALSE)
   exit();
   }
 
+// write comment data to file
 for ($i = 0; $i < count ($res_data); $i++)
   {
   fputcsv ($fh, array (
@@ -92,10 +111,12 @@ for ($i = 0; $i < count ($res_data); $i++)
     $res_data[$i]->{'Text'}
     ));
   }
+
+// close file
 fclose ($fh);
 
+// report success
 echo "Created file: '$fname'\n\n";
-
 exit();
 
 ?>
